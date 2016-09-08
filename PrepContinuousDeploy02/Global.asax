f0a5f4baf6203504d08d32764b83eae1e196f31a
longinus-ekeh <longinus.ekeh@wcbizc.com>
2016-09-08 10:08:46 -0600

    Add project files.

---
+++ PrepContinuousDeploy02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrepContinuousDeploy02.MvcApplication" Language="C#" %>
---
+++ PrepContinuousDeploy02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrepContinuousDeploy02.Startup))]
+namespace PrepContinuousDeploy02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
